574e2df807309aacb100ac28ef12d446852d0975
Anderson Coelho <anderson.coelho@indt.org.br>
2025-01-28 19:16:23 -0400

    create a test of list categories use case

--- app/test/use_cases/test_category_use_case.py
+++ app/test/use_cases/test_category_use_case.py
@@ -1,6 +1,6 @@
 from app.use_cases.category import CategoryUseCase
 from app.db.models import Category as CategoryModel
-from app.schemas.category import Category
+from app.schemas.category import Category, CategoryOutput
 
 def test_add_category_uc(db_session):
     uc = CategoryUseCase(db_session)
@@ -12,10 +12,21 @@ def test_add_category_uc(db_session):
 
     uc.add_category(category=category)
 
-    categories_on_db = db_session.query(CategoryModel).all()
-    assert len(categories_on_db) == 1
-    assert categories_on_db[0].name == 'Clothe'
-    assert categories_on_db[0].slug == 'clothe'
+    categories_db_list = db_session.query(CategoryModel).all()
+    assert len(categories_db_list) == 1
+    assert categories_db_list[0].name == 'Clothe'
+    assert categories_db_list[0].slug == 'clothe'
 
-    db_session.delete(categories_on_db[0])
+    db_session.delete(categories_db_list[0])
     db_session.commit()
+
+def test_list_categories(db_session, categories_on_db):
+    uc = CategoryUseCase(db_session=db_session)
+
+    categories = uc.list_categories()
+
+    assert  len(categories) == 4
+    assert  type(categories[0]) == CategoryOutput
+    assert categories[0].id == categories_on_db[0].id
+    assert categories[0].name == categories_on_db[0].name
+    assert categories[0].slug == categories_on_db[0].slug
