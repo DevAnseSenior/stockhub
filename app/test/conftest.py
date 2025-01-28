566b4c8b2d7483df64377ae014363257a71bb04e
Anderson Coelho <anderson.coelho@indt.org.br>
2025-01-28 19:14:12 -0400

    prepare the test dependencies

--- app/test/conftest.py
+++ app/test/conftest.py
@@ -1,10 +1,33 @@
 import pytest
 from app.db.connection import Session
+from app.db.models import Category as CategoryModels
 
 @pytest.fixture()
 def db_session():
+    session = Session()
     try:
-        session = Session()
         yield session
     finally:
         session.close()
+
+@pytest.fixture()
+def categories_on_db(db_session):
+    categories =  [
+        CategoryModels(name='Clothe', slug='clothe'),
+        CategoryModels(name='Car', slug='car'),
+        CategoryModels(name='Kitchen', slug='kitchen'),
+        CategoryModels(name='Decoration', slug='decoration'),
+    ]
+
+    for category in categories:
+        db_session.add(category)
+    db_session.commit()
+
+    for category in categories:
+        db_session.refresh(category)
+
+    yield categories
+
+    for category in categories:
+        db_session.delete(category)
+    db_session.commit()
