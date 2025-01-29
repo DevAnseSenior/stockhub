fa48cb3dbb8289b956aa5138911bcb6e40976162
Anderson Coelho <anderson.coelho@indt.org.br>
2025-01-28 21:17:57 -0400

    create a test for route list'

--- app/test/routes/test_category_routes.py
+++ app/test/routes/test_category_routes.py
@@ -2,6 +2,7 @@ from fastapi.testclient import TestClient
 from fastapi import status
 from app.db.models import Category as CategoryModel
 from app.main import app
+from app.test.conftest import categories_on_db
 
 client = TestClient(app)
 
@@ -15,7 +16,21 @@ def test_add_category_route(db_session):
 
     assert response.status_code == status.HTTP_201_CREATED
 
-    categories_on_db = db_session.query(CategoryModel).all()
-    assert len(categories_on_db) == 1
-    db_session.delete(categories_on_db[0])
+    categories_db_list = db_session.query(CategoryModel).all()
+    assert len(categories_db_list) == 1
+    db_session.delete(categories_db_list[0])
     db_session.commit()
+
+
+def test_list_categories_route(categories_on_db):
+    response = client.get('/category/list')
+
+    assert response.status_code == status.HTTP_200_OK
+    data = response.json()
+
+    assert len(data) == 4
+    assert data[0] == {
+        "id": categories_on_db[0].id,
+        "name": categories_on_db[0].name,
+        "slug": categories_on_db[0].slug
+    }
