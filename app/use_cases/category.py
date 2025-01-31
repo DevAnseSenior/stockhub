7a3ae8624eaaab49c18f84bca6ad5d08aad31583
Anderson Coelho <anderson.coelho@indt.org.br>
2025-01-31 10:43:01 -0400

    create delete category use case

--- app/use_cases/category.py
+++ app/use_cases/category.py
@@ -1,6 +1,8 @@
 from sqlalchemy.orm import Session
 from app.db.models import Category as CategoryModel
 from app.schemas.category import Category, CategoryOutput
+from fastapi.exceptions import HTTPException
+from fastapi import status
 
 
 def serialize_category(category_model: CategoryModel):
@@ -26,3 +28,11 @@ class CategoryUseCase:
             for category_model in categories_on_db
         ]
         return categories_output
+
+
+    def delete_category(self, id: int):
+        category_model = self.db_session.query(CategoryModel).filter_by(id=id).first()
+        if not category_model:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Category not found')
+        self.db_session.delete(category_model)
+        self.db_session.commit()
