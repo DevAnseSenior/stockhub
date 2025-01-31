a59bd76c0608550763b491268c3a04bf85b3aca2
Anderson Coelho <anderson.coelho@indt.org.br>
2025-01-31 11:17:24 -0400

    create product model

--- app/db/models.py
+++ app/db/models.py
@@ -1,8 +1,23 @@
-from sqlalchemy import  Column, Integer, String
+from datetime import datetime
+from sqlalchemy import  Column, Integer, String, ForeignKey, DateTime, Float
 from app.db.base import Base
 
 class Category(Base):
     __tablename__ = 'categories'
+
     id = Column('id', Integer, primary_key=True, autoincrement=True)
     name = Column('name', String, nullable=False)
     slug = Column('slug', String, nullable=False)
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    id = Column('id', Integer, primary_key=True, autoincrement=True)
+    name = Column('name', String, nullable=False)
+    slug = Column('slug', String, nullable=False)
+    price = Column('price', Float)
+    stock = Column('stock', Integer)
+    created_at = Column('created_at', DateTime, server_default=datetime.now().isoformat())
+    updated_at = Column('updated_at', DateTime, onupdate=datetime.now())
+    category_id = Column('category_id', ForeignKey('categories.id'), nullable=False)
+
